103506d0face064c5a49238a4cf8fc182efbe689
= <=>
2023-12-06 21:05:48 +0530

    axios changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router";
 import "./App.css";
 import axios from "axios";
-// axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = "http://localhost:3000";
 
 function App() {
   return (
